a89b4e314197a84855719d9fb7ced8ac19f52ca0
Spencer Dixon <spencercdixon@gmail.com>
2017-11-30 20:17:37 -0500

    Adds a version command so people can know what they're currently running

---
+++ cmd/version.go
@@ -0,0 +1,26 @@
+// Copyright © 2017 Spencer Dixon <spencercdixon@gmail.com>
+
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// Version is the currently running version of Exocortex
+const Version = "v0.2.1"
+
+// versionCmd represents the version command
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print version of exo binary",
+	Long:  "All software needs a version... this is Exo's.",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("\n  You're currently running Exocortex %s\n\n  Never stop being curious :-)\n\n", Version)
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(versionCmd)
+}
